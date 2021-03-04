05fc9859a956713a2e630c8712e6c6607c8a23b6
iskaldvind <iskaldvind@gmail.com>
2021-03-04 18:48:50 +0300

    homework updated

--- app/src/main/java/io/iskaldvind/calculator/internal/State.java
+++ app/src/main/java/io/iskaldvind/calculator/internal/State.java
@@ -2,8 +2,7 @@ package io.iskaldvind.calculator.internal;
 
 import java.io.Serializable;
 
-@SuppressWarnings("SpellCheckingInspection")
 public enum State implements Serializable {
     
-    COLLECTING_OPERAND_1, COLLECTING_OPERAND_2, UNARY_SUBRESULT, BINARY_SUBRESULT, STATIC_RESULT, RANDOM_RESULT, ERROR
+    COLLECTING_OPERAND_1, COLLECTING_OPERAND_2, STATIC_RESULT, ERROR
 }
